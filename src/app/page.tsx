9104f52307878be591870dd3376cd3e99cdb3772
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-09-26 09:26:21 +0530

    package: add drizzle-orm @neondatabase/serverless dotenv drizzle-kit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button';
+import { env } from '@/env/client';
 
 const HomePage = () => {
 	return (
 		<div>
 			<h1 className='text-4xl font-bold text-green-500'>Home Page</h1>
 			<Button>Test</Button>
+			{env.NEXT_PUBLIC_APP_BASE_URL}
 		</div>
 	);
 };
