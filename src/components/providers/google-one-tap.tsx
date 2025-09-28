2d11dfad9f57cfd84b96979fe03c8ea9a4e01a0b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-09-28 09:26:58 +0530

    fix: google one tap redirect

--- src/components/providers/google-one-tap.tsx
+++ src/components/providers/google-one-tap.tsx
@@ -1,30 +1,27 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 
 import { oneTap, useSession } from '@/lib/auth-client';
 
 export const GoogleOneTap = () => {
-	const router = useRouter();
 	const { data: session, isPending } = useSession();
 
 	useEffect(() => {
 		const handleOneTap = async () => {
 			await oneTap({
+				callbackURL: '/',
+				cancelOnTapOutside: false,
 				fetchOptions: {
 					headers: {
 						'Referrer-Policy': 'no-referrer-when-downgrade',
-					},
-					onSuccess: () => {
-						router.push('/');
 					},
 				},
 			});
 		};
 
 		if (!isPending && !session) handleOneTap();
-	}, [isPending, router, session]);
+	}, [isPending, session]);
 
 	return null;
 };
