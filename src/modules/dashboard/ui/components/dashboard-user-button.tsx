8c274f36e6fefa10723a81af8c1bd2c1e7bdea13
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-09-28 14:08:19 +0530

    feat: add mobile friendly user button in dashboard

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from 'lucide-react';
 
 import { GeneratedAvatar } from '@/components/generated-avatar';
 import { Avatar, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+	Drawer,
+	DrawerContent,
+	DrawerDescription,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from '@/components/ui/drawer';
 import {
 	DropdownMenu,
 	DropdownMenuContent,
@@ -12,23 +23,80 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { useIsMobile } from '@/hooks/use-mobile';
 import { signOut, useSession } from '@/lib/auth-client';
 
 export const DashboardUserButton = () => {
 	const router = useRouter();
 	const { data: session, isPending } = useSession();
+	const isMobile = useIsMobile();
 
-	if (isPending || !session) return null;
+	const [isLoggingOut, setIsLoggingOut] = useState(false);
 
 	const handleLogout = () => {
+		setIsLoggingOut(true);
+
 		signOut({
 			fetchOptions: {
+				onResponse: () => {
+					setIsLoggingOut(false);
+				},
 				onSuccess: () => {
 					router.push('/sign-in');
 				},
 			},
 		});
 	};
+
+	if (isPending || !session) return null;
+
+	if (isMobile) {
+		return (
+			<Drawer>
+				<DrawerTrigger className='border-border/10 flex w-full items-center justify-between gap-x-2 overflow-hidden rounded-lg border bg-white/5 p-3 hover:bg-white/10'>
+					{session.user.image ? (
+						<Avatar>
+							<AvatarImage src={session.user.image} alt={`${session.user.name}'s profile picture`} />
+						</Avatar>
+					) : (
+						<GeneratedAvatar seed={session.user.name} variant='initials' className='mr-3 size-9' />
+					)}
+
+					<div className='flex min-w-0 flex-1 flex-col gap-0.5 overflow-hidden text-left'>
+						<p className='w-full truncate text-sm'>{session.user.name}</p>
+						<p className='w-full truncate text-xs'>{session.user.email}</p>
+					</div>
+
+					<ChevronDownIcon className='size-4 shrink-0' />
+				</DrawerTrigger>
+
+				<DrawerContent>
+					<DrawerHeader>
+						<DrawerTitle>{session.user.name}</DrawerTitle>
+						<DrawerDescription>{session.user.email}</DrawerDescription>
+					</DrawerHeader>
+
+					<DrawerFooter>
+						<Button variant='outline' onClick={() => {}}>
+							<CreditCardIcon />
+							Billing
+						</Button>
+
+						<Button
+							variant='outline'
+							className='text-destructive'
+							disabled={isLoggingOut}
+							isLoading={isLoggingOut}
+							onClick={handleLogout}
+						>
+							<LogOutIcon />
+							Logout
+						</Button>
+					</DrawerFooter>
+				</DrawerContent>
+			</Drawer>
+		);
+	}
 
 	return (
 		<DropdownMenu>
@@ -65,6 +133,7 @@ export const DashboardUserButton = () => {
 				</DropdownMenuItem>
 
 				<DropdownMenuItem
+					disabled={isLoggingOut}
 					variant='destructive'
 					onClick={handleLogout}
 					className='flex cursor-pointer items-center justify-between'
