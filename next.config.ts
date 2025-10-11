86b729f7601792539e3eb234a18e94e9db894189
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-10-11 13:32:27 +0530

    refactor: home page redirect

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,13 @@ import '@/env/server';
 
 const nextConfig: NextConfig = {
 	devIndicators: false,
+	redirects: async () => [
+		{
+			destination: '/meetings',
+			permanent: true,
+			source: '/',
+		},
+	],
 };
 
 export default nextConfig;
